b35813d1de8c92b6ce523edf1830af79286d53ae
teasacura <teasacuraster@gmail.com>
2018-07-09 14:14:29 -0400

    update HomePage, and update frontend routes

---
+++ src/components/HomePage.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <NavLink
+          to="/topics"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+        >Go to TOPICS</NavLink>
+        <NavLink
+          to="/users"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+        >Go To PROFILE</NavLink>
+        <NavLink
+          to="/"
+          exact
+          style={link}
+          activeStyle={{
+            background: 'darkblue'
+          }}
+        >Home</NavLink>
+      </div>
+    );
+  }
+}
+
+
+export default HomePage;
