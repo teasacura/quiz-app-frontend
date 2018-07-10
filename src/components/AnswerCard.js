8bb2e88e8fbb02d24d6675baa4cd0740acc69578
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-10 13:38:20 -0400

    add dnagerous insert for answercard text:

--- src/components/AnswerCard.js
+++ src/components/AnswerCard.js
@@ -5,7 +5,13 @@ class AnswerCard extends React.Component {
   render() {
     return (
       <div>
-      <ul>{this.props.quesObj.answers.map(ans => <li>{ans.answer}</li>)}</ul>
+      <ul>
+        {this.props.quesObj.answers
+          .map(ans => {
+            return <li dangerouslySetInnerHTML={{__html: ans.answer}}></li>
+          })
+        }
+      </ul>
       <button onClick={this.props.nextQ}>Next Question</button>
       </div>
     )
