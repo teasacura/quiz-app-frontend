fdce5b645db5fc576a15d293def2bda01f0e5f10
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2018-07-12 02:27:23 -0400

    style topic card with dynamic dots

--- src/components/TopicsCard.js
+++ src/components/TopicsCard.js
@@ -5,19 +5,23 @@ class TopicsCard extends Component {
     const { title } = this.props.match.params
     const topic = this.props.topics.find(obj => obj.title === title)
     return (
-      <div>
-          {topic ? (
-            <div>
-              <h1>{topic.title}</h1>
-              <p>{topic.description}</p>
-              <ul>
-                {topic.scores.map(score => {
-                  return <li key={score.id}>{score.score} --- {score.username}</li>
-                })}
-              </ul>
-              <button onClick={this.props.toggleQuiz}>START QUIZ!</button>
-            </div>
-          ) : null}
+      <div id="topic">
+        {topic ? (
+          <div>
+            <h2>{topic.title}</h2>
+            <p>{topic.description}</p>
+            <ul>
+              {topic.scores.map(score => {
+                return (<li key={score.id}>
+                  {score.username}
+                  <span className="dots">....................................</span>
+                  <span>{score.score}</span>
+                </li>)
+              })}
+            </ul>
+            <button onClick={this.props.toggleQuiz}>START QUIZ!</button>
+          </div>
+        ) : null}
       </div>
     );
   }
